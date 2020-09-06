d415a10b9e5a178a6875c39832068e6f8e83931e
Manu Uzkudun <manuuzkudun@hotmail.com>
2020-09-06 19:36:19 +0200

    Implemenets the creation of a new course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,6 +3,13 @@ class CoursesController < ApplicationController
     @courses = Course.order(:title)
   end
 
+  def create
+    professor = Professor.find_or_create_by!(email: params[:email])
+    Course.create!(title: params[:title], professor_id: professor.id)
+
+    redirect_to root_path
+  end
+
   def upvote
     course = Course.find(params[:id])
     course.upvote!
